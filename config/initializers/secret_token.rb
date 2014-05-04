e007379f8d2069ca6f5a197c78cc9b93d9ddccd2
Zoe <zoe.goldfarb@gmail.com>
2014-05-03 23:55:08 -0700

    rails app initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BalanceApp::Application.config.secret_key_base = '08c4d080c769f75132322007aae35f609e0cc5f8a998e85c0eb6d710f9c0ab21e644d4e8b81e3b25cafaa33b19748d6a53917ef1459b05d135c8d0ae42973704'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BalanceApp::Application.config.session_store :cookie_store, key: '_balance_app_session'
